e7014b607de212b5a555d267b194184c0fa23dc3
Ryan Vargas <i.am.ryan.v@gmail.com>
2020-05-19 17:46:07 -0700

    final functionality implemented

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Folder (props) {
     let currentFolder = props.currentFolder
+    console.log(currentFolder)
     return (
         <div>
             <h1>Folder Section</h1>
-            {props.navbar()}
+            {props.navbar(currentFolder)}
             <ul>
                  {props.data.store.notes.map((note,index) => {
                     if(note.folderId === currentFolder) {
